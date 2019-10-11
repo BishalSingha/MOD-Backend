029c015895eaa3963dd75ffde1cc529b48fccddc
BishalSingha <vishal7801@gmail.com>
2019-10-11 14:12:27 +0530

    first commit

---
+++ MOD-TEST/TestUL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MOD_DAL;
+using MOD_BAL;
+using NUnit;
+using NUnit.Framework;
+namespace MOD_TEST
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void GetAll()
+        {
+            UL uL = new UL();
+            IList<UserDtl> user = uL.GetAll();
+            Assert.IsNotNull(user);
+
+        }
+    }
+}
